8ff1d34eee3230c0479e6a9569c93eca5d27bdec
macbook <macbook@macbook-pro-mac.local>
2019-01-14 22:56:28 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by macbook on 14.01.2019.
+//  Copyright © 2019 Anna Torczyńska. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
